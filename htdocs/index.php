30ebcbfb0f5dcac53d7cba779c8bc6ec75300a17
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2017-01-27 22:51:16 -0500

    Added more information to fields.

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,74 +13,115 @@ $f3->set(
    )
 );
 
-function load_db_fields( $f3, $db_name, $fields, $sort_key ) {
+function db_fields_names( $fields, $hidden=false ) {
+   foreach( $fields as $field_key => $field_iter ) {
+      if( 
+         !array_key_exists( 'display', $field_iter ) ||
+         $field_iter['display'] ||
+         $hidden
+      ) {
+         if( array_key_exists( 'name', $field_iter ) ) {
+            yield $field_iter['name'];
+         } else {
+            yield $field_key;
+         }
+      }
+   }
+}
+
+function db_fields_load( $f3, $db_name, $fields, $sort_key, $page=0, $hidden=false ) {
    $row = new DB\SQL\Mapper( $f3->get( 'dsdb' ), $db_name );
    $row->load(
       array(),
       array(
          'order' => $sort_key,
-         'offset' => 0,
-         'limit' => 5,
+         'offset' => $page * $f3->get( 'db_page_size' ),
+         'limit' => $f3->get( 'db_page_size' ),
       )
    );
    $items_array = array();
    while( !$row->dry() ) {
       $item = array();
-      foreach( $fields as $field_iter ) {
-         $item[$field_iter] = $row->$field_iter;
+      foreach( $fields as $field_key => $field_iter ) {
+         if(
+            !array_key_exists( 'display', $field_iter ) ||
+            $field_iter['display'] ||
+            $hidden
+         ) {
+            $item[$field_key] = $row->$field_key;
+         }
       }
       $items_array[] = $item;
       $row->next();
    }
    $f3->set( 'items', $items_array );
-   $f3->set( 'fields', $fields );
+   $f3->set( 'fields', db_fields_names( $fields, $hidden ) );
 }
 
 $f3->route( 'GET /', function( $f3, $params ) {
    
 } );
 
-$f3->route( 'GET /auction_house', function( $f3, $params ) {
+$f3->route( 'GET /auction_house/@page', function( $f3, $params ) {
 
    $f3->set( 'title', 'Auction House' );
 
-   load_db_fields(
+/*
+   db_fields_load(
       $f3,
       'auction_house',
       array(
-         'id',
-         'itemid',
-         'stack',   
-         'seller',  
-         'seller_name',
-         'date',
-         'price',
+         'id' => array(
+            'name' => 'Auction ID',
+         ),
+         'itemid' => array(
+            'Item ID',
+         'stack' => 'Stack Size',
+         'seller' => 'Seller',
+         'seller_name' => 'Seller Name',
+         'date' => array(
+            'name' => 'Date',
+         ),
+         'price' =>,
          'buyer_name',
          'sale',
          'sell_date',
       ),
-      'itemid ASC'
+      'itemid ASC',
+      $f3->get( 'PARAMS.page' )
    );
+*/
 
    echo( \Template::instance()->render( 'templates/data.html' ) );
 
 } );
 
-$f3->route( 'GET /npc', function( $f3, $params ) {
+$f3->route( 'GET /npc/@page', function( $f3, $params ) {
 
    $f3->set( 'title', 'NPCs' );
 
-   load_db_fields(
+   db_fields_load(
       $f3,
       'npc_list',
       array(
-         'npcid',
-         'name',
-         'polutils_name',
-         'pos_rot',
-         'pos_x',
+         'npcid' => array(
+            'name' => 'NPC ID',
+         ),
+         'name' => array(
+            'name' => 'NPC Name',
+         ),
+         'polutils_name' => array(
+            'display' => false,
+         ),
+         'pos_rot' => array(
+            'name' => 'Rotation',
+         ),
+         'pos_x' => array(
+            'name' => 'Position X',
+         ),
       ),
-      'npcid ASC'
+      'npcid ASC',
+      $f3->get( 'PARAMS.page' )
    );
 
    echo( \Template::instance()->render( 'templates/data.html' ) );
