0c4ceef902f3d07d1d3b81aa4078e1cd9696d6cb
indigoparadox <indigo.repo@interfinitdynamics.info>
2017-01-29 17:41:09 -0500

    Added new missing elements to auction class.

--- htdocs/classes/poldb/auction.php
+++ htdocs/classes/poldb/auction.php
@@ -7,12 +7,15 @@ class Auction extends POLDBObject {
    protected $fields = array(
       'id' => array(
          'name' => 'Auction ID',
+         'size' => 10,
       ),
       'itemid' => array(
          'name' => 'Item ID',
+         'size' => 10,
       ),
       'stack' => array(
          'name' => 'Stack Size',
+         'size' => 4,
       ),
       'seller' => array(
          'name' => 'Seller',
@@ -22,20 +25,32 @@ class Auction extends POLDBObject {
       ),
       'date' => array(
          'name' => 'Date',
+         'size' => 10,
       ),
       'price' => array(
          'name' => 'Price',
+         'size' => 5,
       ),
       'buyer_name' => array(
          'name' => 'Buyer Name',
       ),
       'sale' => array(
          'name' => 'Sale',
+         'size' => 10,
       ),
       'sell_date' => array(
-         'name' => 'Sell Date'
+         'name' => 'Sell Date',
+         'size' => 10,
       ),
    );
+
+   protected function reroute( $f3 ) {
+      $f3->reroute( '/auction/@page' );
+   }
+
+   protected function get_db_key() {
+      return 'id';
+   }
 
    protected function get_mapper() {
       return new \DB\SQL\Mapper( $this->get( 'dsdb' ), 'auction_house' );
